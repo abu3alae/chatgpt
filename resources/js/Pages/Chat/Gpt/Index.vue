4d2fbffbaf2aecf6f855aac1b4e58b72c4c4f693
Mhamed Mrini <abu3alae@192.168.1.4>
2024-02-14 00:24:57 +0100

    14- Styling load

--- resources/js/Pages/Chat/Gpt/Index.vue
+++ resources/js/Pages/Chat/Gpt/Index.vue
@@ -28,7 +28,7 @@ const submit = () => {
               {{ message.chat_content[0].content }}
             </Link>
           </li>
-        </template>>
+        </template>
       </ul>
         
     </template>
@@ -44,10 +44,12 @@ const submit = () => {
               class="w-full bg-white rounded-lg" 
               placeholder="Ask me anything !" 
               v-model="form.prompt"
-              @keyup="submit"
+              @keyup.enter="submit"
+              :disabled="from.processing"
             />
             <div class="absolute inset-y-0 right-0 flex items-center pl-3">
               <svg 
+                v-if="!form.processing"
                 xmlns="http://www.w3.org/2000/svg" 
                 viewBox="0 0 512 512"
                 class="h-6 w-6 -ml-7 text-slate-700 mr-2"
@@ -56,6 +58,8 @@ const submit = () => {
 
                 <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/>
               </svg>
+
+              <div class="loading -ml-10" v-if="form.processing"></div>
             </div>
           </div>
         </div>
@@ -63,3 +67,35 @@ const submit = () => {
     </template>
   </ChatLayout>
 </template>
+
+<style scoped>
+.loading {
+  position: relative;
+  left: -9999px;
+  width: 12px;
+  height: 10px;
+  border-radius: 8px;
+  background-color: #9880ff;
+  color: #69072f;
+  box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff, 10014px 0 0 0 #9880ff;
+  animation: loading 1.5s infinite linear;
+} 
+
+@keyframes loading {
+  0% {
+    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #be7c38, 10014px 0 0 0 #9880ff;
+  }
+  25% {
+    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #260b9d, 10014px 0 0 0 #9880ff;
+  }
+  50% {
+    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff, 10014px 0 0 0 #9880ff;
+  }
+  75% {
+    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #260b9d, 10014px 0 0 0 #9880ff;
+  }
+  100% {
+    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 0 #410831, 10014px 0 0 0 #9880ff;
+  }
+}
+</style>
