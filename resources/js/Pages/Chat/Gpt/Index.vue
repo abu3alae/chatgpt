6ca0514c652af7f9998433e5d6477bf7da71b967
Mhamed Mrini <abu3alae@192.168.1.4>
2024-02-13 12:48:26 +0100

    8- Use Inertia Form

--- resources/js/Pages/Chat/Gpt/Index.vue
+++ resources/js/Pages/Chat/Gpt/Index.vue
@@ -1,3 +1,17 @@
+<script setup>
+import ChatLayout from '@/Layouts/ChatLayout.vue';
+import { useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+  prompt: ''
+});
+
+const submit = () => {
+  console.log('Submitted!');
+}
+</script>
+
+
 <template>
   <ChatLayout>
     <template #sidebar>
@@ -10,12 +24,19 @@
       <section class="px-4 top-0">
         <div class="w-full">
           <div class="relative flex-1 flex items-center">
-            <input type="text" class="w-full bg-white rounded-lg" placeholder="Ask me anything !" />
+            <input 
+              type="text" 
+              class="w-full bg-white rounded-lg" 
+              placeholder="Ask me anything !" 
+              v-model="prompt"
+              @keyup="submit"
+            />
             <div class="absolute inset-y-0 right-0 flex items-center pl-3">
               <svg 
                 xmlns="http://www.w3.org/2000/svg" 
                 viewBox="0 0 512 512"
                 class="h-6 w-6 -ml-7 text-slate-700 mr-2"
+                @click="submit"
               >
 
                 <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/>
@@ -27,16 +48,3 @@
     </template>
   </ChatLayout>
 </template>
-
-<script>
-import ChatLayout from '@/Layouts/ChatLayout.vue';
-
-export default {
-  name: 'Index',
-  components: {
-    ChatLayout,
-  },
-
-};
-</script>
-
