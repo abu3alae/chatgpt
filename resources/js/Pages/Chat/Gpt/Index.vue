8c43fc0d76a36ee1d41070ddb3ed043152d5014e
Mhamed Mrini <abu3alae@192.168.1.75>
2024-02-16 10:19:46 +0100

    15- Load chat history

--- resources/js/Pages/Chat/Gpt/Index.vue
+++ resources/js/Pages/Chat/Gpt/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import ChatLayout from '@/Layouts/ChatLayout.vue';
 import { Link, useForm } from '@inertiajs/vue3';
+import Content from '@/Components/Content.vue';
 
 const props = defineProps({
   messages: Array,
@@ -33,7 +34,15 @@ const submit = () => {
         
     </template>
     <div class="w-full flex text-white">
-            
+     <template v-if="chat">
+        <div class="w-full flex h-screen bg-slate-800">
+          <div class="w-full overflow-auto pb-36">
+            <template v-for="(content, index) in chat?.chat_content" :key="index">
+              <Content :content="content" />
+            </template>
+          </div>
+        </div>
+      </template>
     </div>
     <template #form>
       <section class="px-4 top-0">
@@ -45,20 +54,21 @@ const submit = () => {
               placeholder="Ask me anything !" 
               v-model="form.prompt"
               @keyup.enter="submit"
-              :disabled="from.processing"
+              :disabled="form.processing"
             />
             <div class="absolute inset-y-0 right-0 flex items-center pl-3">
               <svg 
-                v-if="!form.processing"
                 xmlns="http://www.w3.org/2000/svg" 
-                viewBox="0 0 512 512"
+                fill="none" 
+                viewBox="0 0 24 24" 
+                stroke-width="1.5" 
+                stroke="currentColor" 
                 class="h-6 w-6 -ml-7 text-slate-700 mr-2"
+                v-if="!form.processing"
                 @click="submit"
               >
-
-                <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
               </svg>
-
               <div class="loading -ml-10" v-if="form.processing"></div>
             </div>
           </div>
@@ -83,19 +93,32 @@ const submit = () => {
 
 @keyframes loading {
   0% {
-    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #be7c38, 10014px 0 0 0 #9880ff;
-  }
-  25% {
-    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #260b9d, 10014px 0 0 0 #9880ff;
-  }
-  50% {
-    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff, 10014px 0 0 0 #9880ff;
-  }
-  75% {
-    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #260b9d, 10014px 0 0 0 #9880ff;
-  }
-  100% {
-    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 0 #410831, 10014px 0 0 0 #9880ff;
-  }
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #be7c30,
+        10014px 0 0 0 #9880ff;
+    }
+    16.667% {
+        box-shadow: 9984px -10px 0 0 #9880ff, 9999px 0 0 0 #260b9d,
+        10014px 0 0 0 #9880ff;
+    }
+    33.333% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff,
+        10014px 0 0 0 #9880ff;
+    }
+    50% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px -10px 0 0 #9880ff,
+        10014px 0 0 0 #9880ff;
+    }
+    66.667% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #6b0a5b,
+        10014px 0 0 0 #9880ff;
+    }
+    83.333% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #fb80ff,
+        10014px -10px 0 0 #9880ff;
+    }
+    100% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #410831,
+        10014px 0 0 0 #9880ff;
+    }
 }
 </style>
